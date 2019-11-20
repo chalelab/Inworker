6fbc39e238cc58e539b5d1a487b4899ea5d41982
Benjamin <chalelab@uninorte.edu.co>
2019-11-20 14:58:25 -0500

    Prueba unitaria al entidad Oferta

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,14 @@
-global.fetch = () => ({ json: () => { } })
+// https://create-react-app.dev/docs/running-tests/#initializing-test-environment
+global.fetch = jest.fn(() => ({ json: jest.fn(() => ({})) }))
+
+//** Simulacion de modulos  */
+jest.mock("firebase", () => ({
+    firestore() {
+        return {
+            collection: () => ({
+                add: jest.fn(),
+                doc: jest.fn(() => ({ set: () => { }, delete: () => { } })),
+            })
+        }
+    }
+}))
